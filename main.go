22a218a64650fa6ed6e7c4e6dc1dbf28924c2105
Christian Jodra <c.jodra14@gmail.com>
2022-05-18 16:43:37 +0200

    linter update

--- main.go
+++ main.go
@@ -1,6 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+var (
+	httpDirectory = "./static"
+	formEndpoint  = "form"
+	helloEndpoint = "hello"
+
+	portNumber = "8080"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir(httpDirectory))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/"+formEndpoint, formHandler)
+	http.HandleFunc("/"+helloEndpoint, helloHandler)
+
+	log.Println("Starting server at port ", portNumber)
+	if err := http.ListenAndServe(":"+portNumber, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(responseWrite http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != ("/" + helloEndpoint) {
+		http.Error(responseWrite, "404 Not found", http.StatusNotFound)
+		return
+	}
+	if request.Method != "GET" {
+		http.Error(responseWrite, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(responseWrite, "Hello!")
+}
+
+func formHandler(responseWrite http.ResponseWriter, request *http.Request) {
 
 }

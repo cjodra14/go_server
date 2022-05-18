3ea071bd7ef74cae742b4741d69cebf8d0befba2
Christian Jodra <c.jodra14@gmail.com>
2022-05-18 17:36:08 +0200

    Main server handlers

--- main.go
+++ main.go
@@ -27,17 +27,49 @@ func main() {
 }
 
 func helloHandler(responseWrite http.ResponseWriter, request *http.Request) {
-	if request.URL.Path != ("/" + helloEndpoint) {
-		http.Error(responseWrite, "404 Not found", http.StatusNotFound)
+	if ok := urlPathComprobation(request, responseWrite, ("/" + helloEndpoint)); !ok {
 		return
 	}
-	if request.Method != "GET" {
-		http.Error(responseWrite, "Method is not supported", http.StatusNotFound)
+
+	if ok := isMethodSupported(request, responseWrite, "GET"); !ok {
 		return
 	}
+
 	fmt.Fprintf(responseWrite, "Hello!")
 }
 
 func formHandler(responseWrite http.ResponseWriter, request *http.Request) {
+	ok := validateForm(request, responseWrite)
+	if !ok {
+		return
+	}
+	fmt.Fprintf(responseWrite, "POST request successful")
+	name := request.FormValue("name")
+	address := request.FormValue("address")
+	fmt.Fprintf(responseWrite, "Name = %s\n", name)
+	fmt.Fprintf(responseWrite, "Address = %s\n", address)
+}
 
+func validateForm(request *http.Request, responseWrite http.ResponseWriter) bool {
+	if err := request.ParseForm(); err != nil {
+		fmt.Fprintf(responseWrite, "ParseForm() err: %v", err)
+		return false
+	}
+	return true
+}
+
+func isMethodSupported(request *http.Request, responseWrite http.ResponseWriter, method string) bool {
+	if request.Method != method {
+		http.Error(responseWrite, "Method is not supported", http.StatusNotFound)
+		return false
+	}
+	return true
+}
+
+func urlPathComprobation(request *http.Request, responseWrite http.ResponseWriter, urlPath string) bool {
+	if request.URL.Path != (urlPath) {
+		http.Error(responseWrite, "404 Not found", http.StatusNotFound)
+		return false
+	}
+	return true
 }
